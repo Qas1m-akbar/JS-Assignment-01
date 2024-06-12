791e695b73a1690f7bc9bab2e4c56d46273bc16c
Qas1m-akbar <qasimktk901@gmail.com>
2024-06-12 09:06:05 -0700

    done As 01

---
+++ script.js
@@ -0,0 +1,35 @@
+let marks = prompt("Enter Your Marks here!");
+
+if (marks>100){
+    
+    alert("Enter Marks Between 0 to 100");
+   
+ }
+
+ 
+
+ else if(marks>=90 && marks<=100){
+            alert("it is your marks and you get A+ grade");
+  }
+
+ else if(marks>=80 && marks<=89){
+            alert("This is your marks and you get B grade");
+  }
+
+ else if(marks>=70 && marks<=79){
+            alert("This is your marks and you get C grade");
+  }
+
+ else if(marks>=60 && marks<=69){
+            alert("This is your marks and you get D grade");
+  }
+
+ else if(marks>=50 && marks<=59){
+            alert("This is your marks and you get E grade");
+  }
+
+
+
+ else {
+            alert("Congrutulation you are Fail");
+  }
